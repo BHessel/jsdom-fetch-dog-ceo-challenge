0851d8b5be468aaa57dd4c31ff21c799282eb3b1
BHessel <hesselfitness@gmail.com>
2020-11-19 18:44:16 -0500

    sorta done

--- src/index.js
+++ src/index.js
@@ -9,6 +9,14 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch('https://dog.ceo/api/breeds/list/all')
         .then(resp => resp.json())
         .then(breed => breedList(breed))
+
+    let letters = document.getElementById('breed-dropdown')
+    letters.addEventListener('change', (event) => { 
+        const letter = event.target.value
+        //call the function
+        //HOW DO WE GET BREED HERE?  DUNNO YET
+        breedListSelect(letter)
+    })
 })
 
 function addDogImages(dogImg){
@@ -34,6 +42,45 @@ function breedList(breed){
 
 //Challenge 4
 
-function breedByLetterHelper(letter){
+//refresh the dog breed list
+//Maybe, as part of the refreshing, we do this junk down herevvv
+//on event select from list
+//take the letter they selected
+//cycle through the list of breeds (already up)
+// ==> I guess that means we grab the parent ul & cycle through the li's?
+//if the current one I'm looking at matches the letter, great.  Leave it be
+//else nuke it.
+
+
+
+function breedListSelect(letter){
+    // location.reload()
+    let list = document.getElementById('dog-breeds')
+    let dogs = list.querySelectorAll('li')
     
+        dogs.forEach(breed => {
+        //console.log(breed.textContent)
+        ///let breedName = breed
+        // console.log(breed)
+        // console.log(breed.innerHTML.charAt(0))
+        if (breed.innerHTML.charAt(0) === letter){
+            //do good stuff
+            console.log("This one's good")
+            //debugger
+        }    else {
+            //delete that sucker
+            // console.log(breed)
+            breed.remove()
+            
+        }
+    })
+    // console.log(list, letter)
+    // debugger
+    // for(const breedKey in breed.message){
+    // let li = document.createElement('li')
+    // li.addEventListener('click', (event) => {
+    //     event.currentTarget.style.color = 'red'})           
+    //     li.innerText = breedKey
+    //     list.appendChild(li)
+    // }
 }
