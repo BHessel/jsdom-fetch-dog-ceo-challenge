20caefae66705f7fde6395b236b312c7e7634197
BHessel <hesselfitness@gmail.com>
2020-11-19 16:21:43 -0500

    challenge1and2done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 console.log('%c HI', 'color: firebrick')
+
+//Challenge 1
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('https://dog.ceo/api/breeds/image/random/4')
+        .then(resp => resp.json())
+        .then(dogImg => addDogImages(dogImg))
+
+    fetch('https://dog.ceo/api/breeds/list/all')
+        .then(resp => resp.json())
+        .then(breed => breedList(breed))
+})
+
+function addDogImages(dogImg){
+    let container = document.getElementById('dog-image-container')
+    dogImg.message.forEach(dogUrl => {    
+    let img = document.createElement('img')
+        img.src = dogUrl
+        container.appendChild(img)
+    })
+}
+
+//Challenge 2
+function breedList(breed){
+    let list = document.getElementById('dog-breeds')
+    for(const breedKey in breed.message){
+        let li = document.createElement('li')
+        li.innerText = breedKey
+        list.appendChild(li)
+    }
+}
+
+//Challenge 3
+
+
